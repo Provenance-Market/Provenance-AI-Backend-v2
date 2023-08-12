7f68c5f98726de464c55da8f41105ea96703d561
aaronjan98 <aaronjan98@gmail.com>
2023-08-12 11:42:08 -0700

    add image generation payment to collections contract

--- src/ProvNFTCollection.sol
+++ src/ProvNFTCollection.sol
@@ -7,12 +7,25 @@ import { ERC721SeaDrop } from "./ERC721SeaDrop.sol";
 contract ProvNFTCollection is ERC721SeaDrop {
     address[] private s_collectionOwner;
 
+    event PayFee(address indexed sender);
+
     constructor(
         string memory _name,
         string memory _symbol,
         address[] memory _allowedSeaDrop
     ) ERC721SeaDrop(_name, _symbol, _allowedSeaDrop) {
         s_collectionOwner = _allowedSeaDrop;
+    }
+
+    function imageGenerationPayment(uint256 _cost, address _owner)
+        public
+        payable
+    {
+        require(
+            msg.value >= _cost,
+            "Insufficient payment amount for AI image generation"
+        );
+        emit PayFee(_owner);
     }
 
     // Getter functions for private variables
