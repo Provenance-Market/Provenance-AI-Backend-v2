5861ee8cb6af4fc50b8c766e5bf92b3e1df10d0a
aaronjan98 <aaronjan98@gmail.com>
2023-08-31 05:27:04 -0700

    switch PaymentSplitter functionality with MultiSig to reduce bytecode size

--- src/ProvNFTFactory.sol
+++ src/ProvNFTFactory.sol
@@ -2,20 +2,15 @@
 pragma solidity ^0.8.17;
 
 import { ProvNFTCollection } from "./ProvNFTCollection.sol";
-import {
-    PaymentSplitter
-} from "openzeppelin-contracts/finance/PaymentSplitter.sol";
 
-contract ProvNFTFactory is PaymentSplitter {
+contract ProvNFTFactory {
     mapping(address => ProvNFTCollection[]) private ownerCollections;
-    address[] private s_payees;
+    address private s_multiSig;
 
     event ProvNFTCreated(address owner, address deployedContract);
 
-    constructor(address[] memory _payees, uint256[] memory _shares)
-        PaymentSplitter(_payees, _shares)
-    {
-        s_payees = _payees;
+    constructor(address _ownersMultiSigWallet) {
+        s_multiSig = _ownersMultiSigWallet;
     }
 
     function createBasicNft(string memory _name, string memory _symbol)
